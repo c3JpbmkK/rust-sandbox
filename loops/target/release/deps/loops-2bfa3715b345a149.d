40475b37684fe3e843b6e96c7c4133a8694210a1
Srinivasan Mohana Arunachalam <s.mohanaarunachalam@reply.de>
2022-10-02 10:24:56 +0200

    Add release files

---
+++ loops/target/release/deps/loops-2bfa3715b345a149.d
@@ -0,0 +1,5 @@
+/home/srini/Personal/rust-sandbox/loops/target/release/deps/loops-2bfa3715b345a149: src/main.rs
+
+/home/srini/Personal/rust-sandbox/loops/target/release/deps/loops-2bfa3715b345a149.d: src/main.rs
+
+src/main.rs:
---
+++ loops/target/release/loops.d
@@ -0,0 +1,1 @@
+/home/srini/Personal/rust-sandbox/loops/target/release/loops: /home/srini/Personal/rust-sandbox/loops/src/main.rs
